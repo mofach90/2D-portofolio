f70731b86d11961c5f2dff693deca2031f5b6712
Mohamed Ayari <ayari.mohamed@web.de>
2025-06-17 15:12:49 -0400

    feat: add animation configurations for sprite loading in main.js

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import { k } from "./kaboom.js";
 k.loadSprite("spreadSheet", "./spreadSheet.png", {
   sliceX: 39,
   sliceY: 31,
-  anims: {},
+  anims: {
+    "idle-down": 936,
+    "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+    "idle-side": 975,
+    "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+    "idle-up": 1014,
+    "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+  },
 });
