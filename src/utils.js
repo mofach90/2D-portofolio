bc0ef3cf592a06f3a3c28ffc0fcd34c572b1e165
Mohamed Ayari <ayari.mohamed@web.de>
2025-06-26 15:30:43 -0400

    Fix displayDialogue function to correctly show text dialogue with interval updates

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
 export function displayDialogue(text, onDisplayEnd) {
-    const dialogueUI = document.getElementById("text-dialogue");
-    const dialogue= document.getElementById("dialogue");
+  const dialogueUI = document.getElementById("text-dialogue");
+  const dialogue = document.getElementById("dialogue");
 
-    dialogueUI.style.display = "block";
+  dialogueUI.style.display = "block";
+  const index = 0;
+  const currentText = "";
+  const intervalRef = setInterval(() => {
+    if (index < text.lenght) {
+      currentText += text[index];
+      dialogue.innerHTML = currentText;
+      index++;
+      return;
+    }
+    clearInterval(intervalRef);
+  }, 5);
 }
