ea32c4651ee10ae60ee0258be69833ef8deea5ba
Mohamed Ayari <ayari.mohamed@web.de>
2025-06-27 17:22:51 -0400

    Fix player spawn position calculation in main scene

--- src/main.js
+++ src/main.js
@@ -61,7 +61,10 @@ k.scene("main", async () => {
     if (layer.name === "spawnpoints") {
       for ( const entity of layer.objects){
         if (entity.name === "player") {
-          
+          player.pos = k.vec2(
+            (map.pos.x + entity.x) * scaleFactor,
+            (map.pos.y + entity.y) * scaleFactor
+          )
         }
       }
 
