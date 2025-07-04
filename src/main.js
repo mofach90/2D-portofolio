53748d3023a2619c1549ee5be4a9b99e6bb076e7
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-04 02:43:42 +0200

    Refactor map initialization and player speed adjustment in main scene

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
   },
 });
+
 k.loadSprite("map", "./map.png");
 
 k.setBackground(k.Color.fromHex("#311047"));
@@ -21,23 +22,26 @@ k.setBackground(k.Color.fromHex("#311047"));
 k.scene("main", async () => {
   const mapData = await (await fetch("./map.json")).json();
   const layers = mapData.layers;
-  const map = k.make([k.sprite("map"), k.pos(0)], k.scale(scaleFactor));
+
+  const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
   const player = k.make([
     k.sprite("spritesheet", { anim: "idle-down" }),
     k.area(new k.Rect(k.vec2(0, 3), 10, 10)),
+
     k.body(),
     k.anchor("center"),
     k.pos(),
     k.scale(scaleFactor),
     {
-      speed: 350,
+      speed: 250,
       direction: "down",
       isInDialogue: false,
     },
     "player",
   ]);
-  for (const layer of layers) {
 
+  for (const layer of layers) {
     if (layer.name === "boundaries") {
       for (const boundary of layer.objects) {
         map.add([
@@ -58,8 +62,10 @@ k.scene("main", async () => {
           });
         }
       }
+
       continue;
     }
+
     if (layer.name === "spawnpoints") {
       for (const entity of layer.objects) {
         if (entity.name === "player") {
@@ -72,9 +78,10 @@ k.scene("main", async () => {
       }
     }
   }
+
   k.onUpdate(() => {
-    k.camPos(player.pos.x, player.pos.y + 100)
-  })
+    k.camPos(player.pos.x, player.pos.y + 100);
+  });
 });
 
 k.go("main");
