8514e123bd88f7b8224fb5bf4a0c09747eba650f
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-07 18:08:15 +0200

    Fix dialogue UI element ID in displayDialogue function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export function displayDialogue(text, onDisplayEnd) {
-  const dialogueUI = document.getElementById("text-dialogue");
+  const dialogueUI = document.getElementById("textbox-container");
   const dialogue = document.getElementById("dialogue");
 
   dialogueUI.style.display = "block";
