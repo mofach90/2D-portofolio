bb59e086470c3f863bfe38274b3df2b4903b8a4a
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-11 11:27:58 +0200

    Update PC dialogue text for clarity and detail

--- src/constant.js
+++ src/constant.js
@@ -1,8 +1,8 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/mofach90" target="_blank">Github</a>!`,
+pc: `I'm a Software Engineer currently working as a Cloud Engineer, but my passion for programming remains strong. I code daily, building cool apps and exploring new ideas. These days, I mostly work with JavaScript and TypeScript, and I've even built a few games with them. Regardless of the language, I just love writing code.  
+Check out my <a href="https://github.com/mofach90" target="_blank">GitHub</a>!`,
+
   "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
 };
