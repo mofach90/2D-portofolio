fb8882bcf24f02123b16916a76ce99795edebd32
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-12 23:23:59 +0200

    Update dialogueData to provide a more detailed description of the Computer Science degree

--- src/constant.js
+++ src/constant.js
@@ -4,5 +4,6 @@ export const dialogueData = {
 pc: `I'm a Software Engineer currently working as a Cloud Engineer, but my passion for programming remains strong. I code daily, building cool apps and exploring new ideas. These days, I mostly work with JavaScript and TypeScript, and I've even built a few games with them. Regardless of the language, I just love writing code.  
 Check out my <a href="https://github.com/mofach90" target="_blank">GitHub</a>!`,
 
-  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
+"cs-degree": `This is my Computer Science degree. I keep it on display because it represents a solid foundation in theory and problem-solving that continues to support my work today.`,
+
 };
