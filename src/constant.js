21e2c638b1b787f78c54786e30773aebb23876f9
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-12 23:24:12 +0200

    Add detailed description for sofa-table dialogue in dialogueData

--- src/constant.js
+++ src/constant.js
@@ -6,4 +6,5 @@ Check out my <a href="https://github.com/mofach90" target="_blank">GitHub</a>!`,
 
 "cs-degree": `This is my Computer Science degree. I keep it on display because it represents a solid foundation in theory and problem-solving that continues to support my work today.`,
 
+"sofa-table": `This is where I unwind and dive into YouTube or brainstorm new project ideas. I also create game programming tutorials—check out <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a> if you're into that kind of thing! You’ll even find a breakdown of how I built this interactive portfolio.`,
 };
