67cd2277b5b19f48aa6aa99dfb3f82dead8b2e52
Mohamed Ayari <ayari.mohamed@web.de>
2025-07-16 03:36:00 +0200

    Add content to dialogue control

--- src/constant.js
+++ src/constant.js
@@ -16,5 +16,11 @@ tv: `That's my TV. Lately, I’ve been deep into watching tech YouTubers like
 bed: `This is where I sleep. Funny enough, some of my best ideas hit me when I'm lying here. 
   If I don't jot them down right away, I can’t sleep — they keep spinning in my head, demanding attention.`,
 resume: `This is my desk and on it is my resume.
-Contact me at mohamed.islem@aayari.com if you have any interesting job opportunities!`
+Contact me at mohamed.islem@aayari.com if you have any interesting job opportunities!`,
+projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
+Kaboom.js by watching this tutorials <a href="https://youtube.com/@jslegenddev" target="_blank">here</a>.`,
+library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
+I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+exit: `If you want to exit the portfolio, just close the tab.`,
 };
